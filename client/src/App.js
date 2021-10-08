da8e9e8f91d10c9c811b71e0c639086e63d3d0d7
Zachary Lederman <zlederma@purdue.edu>
2021-10-08 10:40:24 -0400

    front-end moves

--- client/src/App.js
+++ client/src/App.js
@@ -8,57 +8,47 @@ import getWeb3 from "./getWeb3";
 import AboutPage from "./components/about";
 import {Button} from "react-bootstrap"
 import "./App.css";
+import WishView from "./components/WishFrame";
 
 
 class App extends Component {
-  state = { storageValue: [], web3: null, accounts: null, contract: null };
+  state = {  web3: null, accounts: null, contract: null, hidden: false };
   componentDidMount = async () => {
-      // try {
+      try {
       
       
-      // // Get network provider and web3 instance.
-      // // const web3 = await getWeb3();
+      // Get network provider and web3 instance.
+      const web3 = await getWeb3();
 
-      // // Use web3 to get the user's accounts.
-      // const accounts = await web3.eth.getAccounts();
+      // Use web3 to get the user's accounts.
+      const accounts = await web3.eth.getAccounts();
 
-      // // Get the contract instance.
-      // const networkId = await web3.eth.net.getId();
-      // const deployedNetwork = SimpleTokenContract.networks[networkId];
-      // // const deployedNetworkPayment = PaymentHandlerContract.networks[networkId];
-      // const instance = new web3.eth.Contract(
-      //     SimpleTokenContract.abi,
-      //     deployedNetwork && deployedNetwork.address,
-      // );
-      // this.state.contract = instance
-      // this.state.accounts = accounts;
+      // Get the contract instance.
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = SimpleTokenContract.networks[networkId];
+      // const deployedNetworkPayment = PaymentHandlerContract.networks[networkId];
+      const instance = new web3.eth.Contract(
+          SimpleTokenContract.abi,
+          deployedNetwork && deployedNetwork.address,
+      );
+      this.state.contract = instance
+      this.state.accounts = accounts;
 
-      // // Set web3, accounts, and contract to the state, and then proceed with an
-      // // example of interacting with the contract's methods.
-      // this.setState({ web3, accounts, contract: instance });
-      // } catch (error) {
-      // // Catch any errors for any of the above operations.
-      // alert(
-      //     `Failed to load web3, accounts, or contract. Check console for details.`,
-      // );
-      // console.error(error);
-      // }
+      // Set web3, accounts, and contract to the state, and then proceed with an
+      // example of interacting with the contract's methods.
+      this.setState({ web3, accounts, contract: instance });
+      } catch (error) {
+      // Catch any errors for any of the above operations.
+      alert(
+          `Failed to load web3, accounts, or contract. Check console for details.`,
+      );
+      console.error(error);
+      }
       console.log("yo")
   };
 
   getRandomInt = (max) => {
       return Math.floor(Math.random() * max);
-  };
-  runExampleNFT = async () => {
-      console.log(this.state)
-      const { accounts, contract } = this.state;
-      // mints a token
-      await contract.methods.safeMint(accounts[0],this.getRandomInt(78)).send({ from: accounts[0] });
-      // gets the token id
-      const response = await contract.methods.get_token(accounts[0]).call();
-      console.log(response)
-      // updates state array
-      this.setState({ storageValue: [...this.state.storageValue,response]});
   };
   execPayment = async () => {
       const {accounts, contract } = this.state;
@@ -72,35 +62,18 @@ class App extends Component {
       const response = await contract.methods.getBalanceOfPaymentHandler().call();
       // this.setState({storageValue : [...this.state.storageValue, response]})
   };
-  onClickEventNFT = ()=>{
-      this.runExampleNFT();
-  };
-  onClickEventPayment = () =>{
-      this.execPayment();
-  };
 
+  onClickHide = () => {
+    this.setState({hidden:true})
+  }
   render() {
     return (
       <div className="App">
-        {/* <div>
+        <div>
         <NightSky></NightSky>
-        <CardFooter></CardFooter>
-        </div> */}
-        <AboutPage></AboutPage>
-
-        {/* <Navigation/> */}
-        {/* Navbar for site */}
-        {/* <Switch>
-          <Route exact path='/home' component={Hero}/>
-          <Route path='/purchase' />               
-          <Route path='/about'/> 
-        </Switch> */}
-        {/* routes for each button on the navbar */}
-        {/* <div>
-        <Button onClick={this.onClickEventNFT}>Mint NFT</Button>
-        {this.state.storageValue.toString()}
-        </div> */}
-
+        {this.state.hidden ? <WishView handleClick= {this.execPayment.bind(this)}/> : <AboutPage/>}
+        <CardFooter handleClick={this.onClickHide.bind(this)}></CardFooter>  
+        </div>
       </div>
     );
   }
