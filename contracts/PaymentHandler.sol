809df32fc12dcba7d396876ce45ae51820a5283b
Zachary Lederman <zlederma@purdue.edu>
2021-10-15 18:31:21 -0400

    beginning raffle functionality
    added capacity to enter raffle
    added shuffling algorithm to shuffle entries

--- contracts/PaymentHandler.sol
+++ contracts/PaymentHandler.sol
@@ -7,37 +7,46 @@ import "./MyWish.sol";
 //Need to set up ownable for contract
 //Need to create reserve function
 
-contract PaymentHandler is PaymentSplitter {
+contract PaymentHandler is PaymentSplitter{
 
-    mapping(address => uint256) balances;
-    uint256 constant PRICE = 3000000000000000000 wei;
-    event fundsAccepted(address from);
-    uint256 totalSales;
+    using Counters for Counters.Counter; 
+
+    uint16 immutable zachIndex = 0;
+    uint16 immutable evanIndex = 2;
+    uint16 immutable sebIndex = 1;
+    uint16 immutable wishIndex = 3;
+
+    uint256 immutable PRICE = 3000000000000000000 wei;
+    uint64 immutable TOKEN_CAP = 10000;
+    uint16 immutable USER_TOKEN_CAP = 5;
+
+    bool raffleIsActive = true;
     bool saleIsActive = true;
-    
-    /*
-        Need to add values to the following variables
-    */
+    bool entropySet = false;
+
+    Counters.Counter private raffleCount;       
+
     MyWish _myWishContract;
 
-    uint16 zachIndex = 0;
-    uint16 evanIndex = 2;
-    uint16 sebIndex = 1;
-    uint16 wishIndex = 3;
-
     //Make Shares out of 100
-    uint16 _SebShares = 2;
-    uint16 _ZachShares = 2;
-    uint16 _EvanShares = 6;
-    uint16 _MakeAWishShares = 90;
+    uint16 sebShares = 2;
+    uint16 zachShares = 2;
+    uint16 evanShares = 6;
+    uint16 makeAWishShares = 90;
+    uint256 entropy;
+
     address makeAWish = address(0);
+    uint256[] shares_ = [sebShares, zachShares, evanShares, makeAWishShares];
 
-    uint256[] shares_ = [_SebShares, _ZachShares, _EvanShares, _MakeAWishShares];
-
+    mapping(address => uint16) tokensPerUser;
+    address[] raffleEntries;
+    
+    event fundsAccepted(address from);
+    event enteredRaffle(address from ,uint16 numTokens);
+    event raffleShuffled(uint256 totalEntries);
 
     constructor(address tokenAddress,address[] memory payees) PaymentSplitter(payees, shares_) {
         _myWishContract = MyWish(tokenAddress);
-        totalSales = 0;
         makeAWish = payees[3];
     }
 
@@ -51,13 +60,52 @@ contract PaymentHandler is PaymentSplitter {
         require(msg.value == PRICE,"Value Sent Error");
         require(msg.sender != address(0),"Invalid Address");
 
-        bool success = processPurchase(msg.sender);
+        bool success = _myWishContract.createCollectable(msg.sender);
         require(success,"Token Not Minted Error");
         releaseEther();
-        totalSales +=1;
-
 
     }
+
+
+
+    function enterRaffle(uint16 numTokens)
+    public 
+    payable
+    {
+        require(raffleIsActive, "Raffle is not active");
+        require(msg.value == numTokens * PRICE,"Not enough ETH sent");
+        require(numTokens + raffleCount.current() < TOKEN_CAP,"Please request fewer tokens");
+        require(tokensPerUser[msg.sender] + numTokens < USER_TOKEN_CAP,"Please request fewer tokens");
+
+        tokensPerUser[msg.sender] += numTokens;
+        for(uint i = 0; i < numTokens; i++){
+            raffleCount.increment();
+        }
+        raffleEntries.push(msg.sender);
+        emit enteredRaffle(msg.sender, numTokens);
+
+    }
+    
+    function shuffleEntries() public {
+        require(!raffleIsActive,"Raffle Is Still Active");
+        require(raffleEntries.length > TOKEN_CAP,"No need to shuffle");
+        require(entropySet, "No randomness to shuffle with");
+
+        for (uint256 i = 0; i < raffleEntries.length; i++) {
+            // Generate a random index to select from
+            uint256 randomIndex = i + entropy % (raffleEntries.length - i);
+            // Collect the value at that random index
+            address randomTmp = raffleEntries[randomIndex];
+            // Update the value at the random index to the current value
+            raffleEntries[randomIndex] = raffleEntries[i];
+            // Update the current value to the value at the random index
+            raffleEntries[i] = randomTmp;
+        }
+        emit raffleShuffled(raffleEntries.length);
+
+    }
+
+
 
     function getTokenAddress() external view returns(address) {
         return address(_myWishContract);
@@ -65,19 +113,12 @@ contract PaymentHandler is PaymentSplitter {
 
     //does this return the total balance in the contract? 
     function getBalanceOfPaymentHandler()
-   
     external
     view
     returns(uint256){
         return address(this).balance;
     }
 
-
-    function getPrice() 
-    external pure
-    returns (uint256){
-        return PRICE;
-    }
   
     function releaseEther() internal {
         address payable zach = payable(payee(zachIndex));
@@ -97,17 +138,19 @@ contract PaymentHandler is PaymentSplitter {
     }
 
 
-    function processPurchase(address beneficiary) internal returns(bool) {
 
-        bool success = _myWishContract.createCollectable(beneficiary);
-        require(success,"Token Contract Did Not Mint Token");
 
-        return success == true;
+    function getTokensRequested(address user) public view returns(uint16) {
+        require(user != address(0),"invalid address");
+        require(tokensPerUser[user] > 0, "No Tokens Requested");
+        
+        return tokensPerUser[user];
     }
 
-    function getTotalSales() public view returns (uint256) {
-            return totalSales;
+    function getPrice() public pure returns(uint256){
+        return PRICE;
     }
+
 
 
 
