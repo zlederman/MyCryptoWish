fc59b1adfe80b76d15289d1a19c86dc59cb2de1f
Zachary Lederman <zlederma@purdue.edu>
2021-11-08 17:24:12 -0500

    update server
    redis is working but probably insecure

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const path = require('path');
-const WebSocket = require('ws')
+const WebSocket = require('ws');
+
 const web3 = require('web3');
-const redis = require('redis')
+const redis = require('redis');
+require('dotenv').config({ path: '/Users/zlederman/Documents/Code/js/truffle-react/.env' })
 const schedule = require('node-schedule');
 const appSockets = express();
+const util = require('util');
+
 const appServer = express();
 
-const redisClient = redis.createClient('6379','127.0.0.1');
-const CONTRACT_ADDRESS = "0x5A22534eD9aA1f35A284b748459e250fb159A163"; //change before running server
+const redisClient = redis.createClient(process.env.REDIS_PORT,'127.0.0.1');
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;//change before running server
 const CONTRACT_ABI = require('./client/src/contracts/PaymentHandler.json'); 
-const RAFFLE_TOPIC = "0x0caab1a325caae32148db4a8c14466a83a84b5e074b83e4dced52ea46c6b003f" 
-const WSS_URL = "wss://kovan.infura.io/ws/v3/123d564490cd4062800d63c13c221bd5"
+const RAFFLE_TOPIC = process.env.RAFFLE_TOPIC;
+const WSS_URL = process.env.WSS_URL;
 
 const CONTRACT_OPTIONS = [
     "logs",
@@ -32,6 +36,7 @@ redisClient.on('connect',()=>{
     console.log('connected to redis');
     redisClient.ping()
 })
+redisClient.get = util.promisify(redisClient.get);
 // const contractInstance = web3Instance.eth.Contract(CONTRACT_ABI,CONTRACT_ADDRESS);
 
 
@@ -90,7 +95,7 @@ ws.onopen = function(){
     console.log("opened connection to eth-node")
     ws.send(JSON.stringify(WS_OPTIONS))
 }
-ws.onmessage = function (evt) {
+ws.onmessage = async function (evt) {
     console.log('msg recieved from eth-node')
     json = JSON.parse(evt.data)
     if(json['params']){
@@ -98,7 +103,7 @@ ws.onmessage = function (evt) {
         data = res.data.substr(2);
         payload = parse(data);
         console.log(payload)
-        redisClient.set(
+        await redisClient.set(
            payload.address,
            payload.raffleEntries.toString()
         )
@@ -118,10 +123,9 @@ const parse = (data) => {
     for (let i = 0, o = 0; i < numChunks; ++i, o += size) {
         chunks[i] = data.substr(o, size)
     } 
-    address = "0x" + parseInt(chunks[0],16).toString(16);
+    address = "0x" + chunks[0].substr(24);
     numEntries = parseInt(chunks[1],16);
     lastIndex = parseInt(chunks[2],16);
-
     for(let i = 0; i < numEntries; i++){
         raffleEntries.push(--lastIndex);
     }
@@ -136,15 +140,15 @@ ws.onclose = function () {
 
 
 
-appServer.get('getraffletix', (req,res) => {
-    address = req.body.address;
-    raffleTickets = redisClient.get(address).split(",");
+appServer.get('/raffle', async (req,res) => {
+    address = req.query.id.toLowerCase();
+    raffleTickets = await redisClient.get(address);
     res.send(JSON.stringify({tickets: raffleTickets}))
 })
 
 
 appServer.listen(8080,console.log('appServer listening on port 8080'));
-appSockets.listen(430,console.log('appSockets listenin on port 430'));
+
 
 
 
