887962a92ad69d9bd035a9ff0f598d3d8237f713
Zachary Lederman <zlederma@purdue.edu>
2021-11-08 12:30:09 -0500

    server is working

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const HDWalletProvider = require('@truffle/hdwallet-provider')
 const path = require('path');
 const WebSocket = require('ws')
 const web3 = require('web3');
@@ -7,10 +6,10 @@ const schedule = require('node-schedule');
 const appSockets = express();
 const appServer = express();
 
-const CONTRACT_ADDRESS = "0x7b83B171c3d75b35276BD379BF48249119FC9E29";
-const CONTRACT_ABI = require('./client/src/contracts/PaymentHandler.json');
-const RAFFLE_TOPIC = "0x0caab1a325caae32148db4a8c14466a83a84b5e074b83e4dced52ea46c6b003f"
-
+const CONTRACT_ADDRESS = "0x5D37B393889185c14A4aA25D9d87FcBfc8d696Be"; //change before running server
+const CONTRACT_ABI = require('./client/src/contracts/PaymentHandler.json'); 
+const RAFFLE_TOPIC = "0x0caab1a325caae32148db4a8c14466a83a84b5e074b83e4dced52ea46c6b003f" 
+const WSS_URL = "wss://kovan.infura.io/ws/v3/123d564490cd4062800d63c13c221bd5"
 const CONTRACT_OPTIONS = [
     "logs",
     {
@@ -24,11 +23,9 @@ const WS_OPTIONS = {
     "method":"eth_subscribe",
     "params":CONTRACT_OPTIONS
 }
-const web3Instance = new web3(
-    new Web3.providers.WebsocketProvider(process.env.WSS_URL)
-);
-const ws = new WebSocket(process.env.WSS_URL);
-const contractInstance = web3Instance.eth.Contract(CONTRACT_ABI,CONTRACT_ADDRESS);
+
+const ws = new WebSocket(WSS_URL);
+// const contractInstance = web3Instance.eth.Contract(CONTRACT_ABI,CONTRACT_ADDRESS);
 
 
 async function send(web3, gasPrice, transaction) {
@@ -53,18 +50,30 @@ const dates = {
 }
 
 const setRaffleJob = schedule.scheduleJob(dates['raffle'],async() =>{
+    const web3Instance = new web3(
+        new Web3.providers.WebsocketProvider(process.env.WSS_URL)
+    );
     let transaction = contractInstance.methods.setState(2);
     let receipt = await send(web3Instance,25000,transaction);
 })
 const setPreMintJob = schedule.scheduleJob(dates['premint'],async() =>{
+    const web3Instance = new web3(
+        new Web3.providers.WebsocketProvider(process.env.WSS_URL)
+    );
     let transaction = contractInstance.methods.setState(3);
     let receipt = await send(web3Instance,25000,transaction);
 })
 const setMintJob = schedule.scheduleJob(dates['mint'],async() =>{
+    const web3Instance = new web3(
+        new Web3.providers.WebsocketProvider(process.env.WSS_URL)
+    );
     let transaction = contractInstance.methods.setState(4);
     let reciept = await send(web3Instance,25000,transaction);
 })
 const setOpenJob = schedule.scheduleJob(dates['open'],async ()=>{
+    const web3Instance = new web3(
+        new Web3.providers.WebsocketProvider(process.env.WSS_URL)
+    );
     let transaction = contractInstance.methods.setState(5);
     let reciept = await send(web3Instance,25000,transaction);
 })
@@ -72,33 +81,42 @@ const setOpenJob = schedule.scheduleJob(dates['open'],async ()=>{
 
 ws.onopen = function(){
     console.log("opened connection to eth-node")
-    ws.send(`${WS_OPTIONS}`)
+    ws.send(JSON.stringify(WS_OPTIONS))
 }
 ws.onmessage = function (evt) {
+    console.log('msg recieved from eth-node')
     json = JSON.parse(evt.data)
-    const size = 64
-    const numChunks = 3
-    let chunks = Array(3)
     if(json['params']){
         res = json.params.result
         data = res.data.substr(2);
-        for (let i = 0, o = 0; i < numChunks; ++i, o += size) {
-            chunks[i] = data.substr(o, size)
-        }
-        address = '0x' + chunks[0];
-        numEntries = int(chunks[1]);
-        lastIndex = int(chunks[2]);
-        raffleEntries = Array(numEntries);
-        for(let i = 0; i < numEntries; i++){
-            tokenIds.push(--lastIndex);
-        }
-        payload = {address: address, raffleEntries: raffleEntries};
+        payload = parse(data);
         console.log(payload)
+
         //post payload to db
     }
+}
+setInterval(()=>{
+    ws.ping()
+},3.54e6)
 
+const parse = (data) => {
+    const size = 64
+    const numChunks = 3
+    let chunks = Array(3)
+    for (let i = 0, o = 0; i < numChunks; ++i, o += size) {
+        chunks[i] = data.substr(o, size)
+    }
+    address = '0x' + chunks[0];
+    numEntries = parseInt(chunks[1]);
+    lastIndex = parseInt(chunks[2]);
+    raffleEntries = Array(numEntries);
+    for(let i = 0; i < numEntries; i++){
+        raffleEntries.push(--lastIndex);
+    }
+    payload = {address: address, raffleEntries: raffleEntries};
+    return payload;
 
-};
+}
 ws.onclose = function () {
     // websocket is closed.
     console.log("Connection closed...");
